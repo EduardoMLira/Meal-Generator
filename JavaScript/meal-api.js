b7231b03fa012fa3d77b7cf529c67deda39d7ee2
DeLira12 <marquesliraeduardo@gmail.com>
2023-09-23 15:00:05 -0300

    feat: Adiciona melhorias e informações no layout.

--- JavaScript/meal-api.js
+++ JavaScript/meal-api.js
@@ -1,13 +1,34 @@
-const category = document.getElementById('category')
+const mealName = document.getElementById('mealName')
 const mealImg = document.getElementById('mealImg')
+const category = document.getElementById('category')
+const area = document.getElementById('area')
+const tags = document.getElementById('tags')
+const mealVideo = document.getElementById('mealVideo')
+const instructions = document.getElementById('instructions')
+const ingredientList = document.getElementById('ingredientList')
 
 fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then((response) => response.json())
     .then((randomMeal) => {
         const meal = randomMeal.meals[0]
 
-        category.textContent = meal.strCategory
+        mealName.textContent = meal.strMeal
         mealImg.src = meal.strMealThumb
+        category.textContent = meal.strCategory
+        area.textContent = meal.strArea
+        tags.textContent = meal.strTags
+        mealVideo.src = meal.strYoutube
+        instructions.textContent = meal.strInstructions
+
+        for (let i = 1; i <= 20; i++) {
+            const ingredientKey = `strIngredient${i}`;
+            const ingredientValue = meal[ingredientKey];
+        
+            if (ingredientValue) {
+                const ingredientElement = document.createElement('li');
+                ingredientElement.textContent = `${ingredientValue};`;
+                ingredientList.appendChild(ingredientElement);
+            }
+        }
     })
     .catch((error) => console.log(error))
-
